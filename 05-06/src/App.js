cb6a6b326925be93cfe767c718fdf82fb7523a67
IvanFerroli <ivanilson.ferreira.mec@gmail.com>
2023-06-06 17:15:31 -0300

    feat: first exercise complete

--- 05-06/src/App.js
+++ 05-06/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
 function App() {
+  const [counterValue, setCounterValue] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <body className="App-body">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click on the minus (-) or plus (+) buttons to increase or decrease the counter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter counterValue={counterValue} setCounterValue={setCounterValue}/>
+      </body>
     </div>
   );
 }
 
 export default App;
+
+function Counter(props) {
+  let counterValue = props.counterValue
+  let setCounterValue = props.setCounterValue
+  return(
+    <>
+      <button className="minus" onClick={() => setCounterValue(counterValue - 1)} >-</button>
+      <p>{counterValue}</p>
+      <button className="plus" onClick={() => setCounterValue(counterValue + 1)}>+</button>
+    </>
+  )
+}
+
+function HandleClick() {
+
+}
